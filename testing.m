b9b252793ed58e6fcb532457b9051bd226f04deb
SanAndriuwa <37011046+SanAndriuwa@users.noreply.github.com>
2026-09-10 18:18:27 +0300

    Use English comments in simple neural-network examples

--- testing.m
+++ testing.m
@@ -4,38 +4,38 @@ target = (1 + 0.6*sin(2*pi*x/0.7) + 0.3*sin(2*pi*x)) / 2;
 epoch = 10000;
 eta = 0.1;
 
-% Первый слой.
+% Hidden layer.
 w11_1 = rand(1);
 b1_1 = 0;
 w21_1 = rand(1);
 b2_1 = 0;
-% Второй слой.
+% Output layer.
 w11_2 = rand(1);
 w21_2 = rand(1);
 b1_2 = 0;
 for j = 1:epoch
     for i = 1:length(x)
-        % Прямой проход.
+        % Forward pass.
         v1_1 = x(i)*w11_1 + b1_1;
         y1_1 = tanh(v1_1);
         v2_1 = x(i)*w21_1 + b2_1;
         y2_1 = tanh(v2_1);
         v1_2 = y1_1*w11_2 + y2_1*w21_2 + b1_2;
         y1_2 = v1_2;
-        % Ошибка.
+        % Output error.
         e = target(i) - y1_2;
 
-        % Delta считаем до обновления весов.
+        % Calculate all deltas before updating weights.
         delta1_2 = e;
         delta1_1 = (1 - y1_1^2)*delta1_2*w11_2;
         delta2_1 = (1 - y2_1^2)*delta1_2*w21_2;
 
-        % Обновление выходного слоя.
+        % Update output weights and bias.
         w11_2 = w11_2 + eta*delta1_2*y1_1;
         w21_2 = w21_2 + eta*delta1_2*y2_1;
         b1_2 = b1_2 + eta*delta1_2;
 
-        % Обновление скрытого слоя.
+        % Update hidden weights and biases.
         w11_1 = w11_1 + eta*delta1_1*x(i);
         b1_1 = b1_1 + eta*delta1_1;
         w21_1 = w21_1 + eta*delta2_1*x(i);
@@ -44,7 +44,7 @@ for j = 1:epoch
 
 end
 
-% Проверка после обучения.
+% Test after training.
 x_naujas = 0.1:1/22:1;
 target_naujas = (1 + 0.6*sin(2*pi*x_naujas/0.7) ...
     + 0.3*sin(2*pi*x_naujas)) / 2;
