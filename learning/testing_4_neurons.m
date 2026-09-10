21803d28f5fc9cd1a37bac69203baf237d330233
SanAndriuwa <37011046+SanAndriuwa@users.noreply.github.com>
2026-09-10 18:18:36 +0300

    Use English comments in simple neural-network examples

--- learning/testing_4_neurons.m
+++ learning/testing_4_neurons.m
@@ -4,7 +4,7 @@ target = (1 + 0.6*sin(2*pi*x/0.7) + 0.3*sin(2*pi*x)) / 2;
 epoch = 10000;
 eta = 0.1;
 
-% Первый слой.
+% Hidden layer.
 w11_1 = rand(1);
 b1_1 = 0;
 w21_1 = rand(1);
@@ -13,7 +13,7 @@ w41_1 = rand(1);
 b2_1 = 0;
 b3_1 = 0;
 b4_1 = 0;
-% Второй слой.
+% Output layer.
 w11_2 = rand(1);
 w21_2 = rand(1);
 w31_2 = rand(1);
@@ -21,7 +21,7 @@ w41_2 = rand(1);
 b1_2 = 0;
 for j = 1:epoch
     for i = 1:length(x)
-        % Прямой проход.
+        % Forward pass.
         v1_1 = x(i)*w11_1 + b1_1;
         y1_1 = tanh(v1_1);
         v2_1 = x(i)*w21_1 + b2_1;
@@ -32,24 +32,24 @@ for j = 1:epoch
         y4_1 = tanh(v4_1);
         v1_2 = y1_1*w11_2 + y2_1*w21_2 + y3_1*w31_2 + y4_1*w41_2 + b1_2;
         y1_2 = v1_2;
-        % Ошибка.
+        % Output error.
         e = target(i) - y1_2;
 
-        % Delta считаем до обновления весов.
+        % Calculate all deltas before updating weights.
         delta1_2 = e;
         delta1_1 = (1 - y1_1^2)*delta1_2*w11_2;
         delta2_1 = (1 - y2_1^2)*delta1_2*w21_2;
         delta3_1 = (1 - y3_1^2)*delta1_2*w31_2;
         delta4_1 = (1 - y4_1^2)*delta1_2*w41_2;
 
-        % Обновление выходного слоя.
+        % Update output weights and bias.
         w11_2 = w11_2 + eta*delta1_2*y1_1;
         w21_2 = w21_2 + eta*delta1_2*y2_1;
         w31_2 = w31_2 + eta*delta1_2*y3_1;
         w41_2 = w41_2 + eta*delta1_2*y4_1;
         b1_2 = b1_2 + eta*delta1_2;
 
-        % Обновление скрытого слоя.
+        % Update hidden weights and biases.
         w11_1 = w11_1 + eta*delta1_1*x(i);
         b1_1 = b1_1 + eta*delta1_1;
         w21_1 = w21_1 + eta*delta2_1*x(i);
@@ -62,7 +62,7 @@ for j = 1:epoch
 
 end
 
-% Проверка после обучения.
+% Test after training.
 x_naujas = 0.1:1/22:1;
 target_naujas = (1 + 0.6*sin(2*pi*x_naujas/0.7) ...
     + 0.3*sin(2*pi*x_naujas)) / 2;
